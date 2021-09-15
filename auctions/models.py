cc36f08da279711634c5183476e92916b220fd32
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-09-15 23:53:42 +0200

    Add str auction list

--- auctions/models.py
+++ auctions/models.py
@@ -14,10 +14,13 @@ class AuctionList(models.Model):
     ('HG','Higiene'),
     ('NO', 'No category')
     ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)# no guarda el id sino el user
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='NO')
     
     image = models.URLField()
     name = models.CharField(max_length=100)
     description = models.TextField()
     bid = models.FloatField()#starting big
+
+    def __str__(self):
+        return f"Aticle: {self.id} name: {self.name}, seller: {self.user}"
