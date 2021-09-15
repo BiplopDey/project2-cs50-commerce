24d610ce012a6a88936da18db19675d85c002519
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-09-15 23:44:20 +0200

    Add auctionList model, not migrated yet

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionList(models.Model):
+    CATEGORY_CHOICES = [
+    ('ELE','Electronica'),
+    ('CO','Cocina'),
+    ('RP','Ropa'),
+    ('HG','Higiene'),
+    ('NO', 'No category')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    
+    image = models.URLField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    bid = models.FloatField()#starting big
